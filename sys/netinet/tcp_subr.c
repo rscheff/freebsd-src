5349956fd19cc4b8b4230d0b9bba75494f35c762
Richard Scheffenegger <rscheff@FreeBSD.org>
2021-02-14 10:09:39 +0100

    typo

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1763,7 +1763,7 @@ tcp_newtcpcb(struct inpcb *inp)
 	callout_init(&tp->t_timers->tt_2msl, 1);
 	callout_init(&tp->t_timers->tt_delack, 1);
 
-	if !(tp->t_flags & TF_NOOPT) {
+	if (!(tp->t_flags & TF_NOOPT)) {
 		if (V_tcp_do_rfc1323)
 			tp->t_flags = (TF_REQ_SCALE|TF_REQ_TSTMP);
 		if (V_tcp_do_sack)
