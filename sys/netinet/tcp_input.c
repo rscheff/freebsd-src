6b859a8b8e35d3c9f2679d467d90b3d47e89681f
Richard Scheffenegger <rscheff@FreeBSD.org>
2021-02-19 12:45:44 +0100

    sprinkling logging

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -2609,6 +2609,8 @@ tcp_do_segment(struct mbuf *m, struct tcphdr *th, struct socket *so,
 					tp->t_dupacks = 0;
 				else if (++tp->t_dupacks > tcprexmtthresh ||
 				     IN_FASTRECOVERY(tp->t_flags)) {
+				        if (tp->t_inpcb->inp_socket->so_options & SO_DEBUG)
+					    log(LOG_CRIT, ">dupthresh: %d dupacks, %d recover_fs\n", tp->t_dupacks, tp->sackhint.recover_fs);
 					cc_ack_received(tp, th, nsegs,
 					    CC_DUPACK);
 					if (V_tcp_do_prr &&
@@ -2647,6 +2649,9 @@ tcp_do_segment(struct mbuf *m, struct tcphdr *th, struct socket *so,
 					     tp->sackhint.sacked_bytes >
 					     (tcprexmtthresh - 1) * maxseg)) {
 enter_recovery:
+					if (tp->t_inpcb->inp_socket->so_options & SO_DEBUG)
+					    log(LOG_CRIT, "==dupthresh: %d dupacks\n", tp->t_dupacks);
+
 					/*
 					 * Above is the RFC6675 trigger condition of
 					 * more than (dupthresh-1)*maxseg sacked data.
@@ -2791,6 +2796,8 @@ enter_recovery:
 			    (to.to_flags & TOF_SACK) &&
 			    sack_changed) {
 				tp->t_dupacks++;
+				if (tp->t_inpcb->inp_socket->so_options & SO_DEBUG)
+					log(LOG_CRIT, "full ack+sack: %d dupacks\n", tp->t_dupacks);
 				/* limit overhead by setting maxseg last */
 				if (!IN_FASTRECOVERY(tp->t_flags) &&
 				    (tp->sackhint.sacked_bytes >
@@ -4006,11 +4013,13 @@ tcp_do_prr_ack(struct tcpcb *tp, struct tcphdr *th, struct tcpopt *to)
 	} else if (IN_CONGRECOVERY(tp->t_flags))
 		tp->snd_cwnd = imax(maxseg, pipe - del_data +
 				    (snd_cnt * maxseg));
-if(tp->snd_cwnd < maxseg) {
-log(2, "PRR low cwnd:%d nxt:%d recover:%d sackrexmit:%d snd_cnt:%ld\n",
-tp->snd_cwnd, tp->snd_nxt - tp->iss, tp->snd_recover-tp->iss,
-tp->sackhint.sack_bytes_rexmit, snd_cnt);
-}
+	if ((tp->t_inpcb->inp_socket->so_options & SO_DEBUG) ||
+	    (tp->snd_cwnd < maxseg)) {
+		log(LOG_CRIT, "PRR low cwnd:%d nxt:%d recover:%d sackrexmit:%d snd_cnt:%ld del_data:%ld pipe:%ld limit:%ld ack:%d una:%d fack:%d ssthresh:%d recover_fs:%d\n",
+		tp->snd_cwnd, tp->snd_nxt - tp->iss, tp->snd_recover-tp->iss,
+		tp->sackhint.sack_bytes_rexmit, snd_cnt, del_data, pipe, limit,
+		th->th_ack-tp->iss, tp->snd_una-tp->iss, tp->snd_fack-tp->iss, tp->snd_ssthresh, tp->sackhint.recover_fs);
+	}
 }
 
 /*
