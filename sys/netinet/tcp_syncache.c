f3d6045b7c4224231468d8daa570e1bd5d0478d8
Richard Scheffenegger <rscheff@FreeBSD.org>
2022-02-03 21:51:04 +0100

    Add/update AccECN related statistics and numbers
    Summary:
    Reserve couters in the tcps struct in preparation
    for AccECN, extend the debugging output for TF2
    flags, optimize the syncache flags from individual
    bits to a codepoint for the specifc ECN handshake.
    
    This is in preparation of AccECN.
    
    No functional chance except for extended debug
    output capabilities.
    
    Reviewers: #netapp, #transport!, hselasky, rrs, tuexen, glebius, jtl, kbowling
    
    Subscribers: imp, melifaro
    
    Differential Revision: https://reviews.freebsd.org/D34161

--- sys/netinet/tcp_syncache.c
+++ sys/netinet/tcp_syncache.c
@@ -999,7 +999,8 @@ syncache_socket(struct syncache *sc, struct socket *lso, struct mbuf *m)
 			tp->t_flags |= TF_SACK_PERMIT;
 	}
 
-	tcp_ecn_syncache_socket(tp, sc);
+	if (sc->sc_flags & SCF_ECN)
+		tp->t_flags2 |= TF2_ECN_PERMIT;
 
 	/*
 	 * Set up MSS and get cached values from tcp_hostcache.
